47c3487f2a257691f0634e8622f10f957a1d5d27
salhindi <calisaraalhindi@gmail.com>
2021-08-23 17:12:21 -0700

    travel plan class

---
+++ src/travel_plan.js
@@ -0,0 +1,11 @@
+class TravelPlan {
+    constructor(id, travelPlanAttributes) {
+        this.id =id;
+        this.name = travelPlanAttributes.name;
+        this.start_date = travelPlanAttributes.start_date;
+        this.end_date = travelPlanAttributes.end_date;
+        this.destination = travelPlanAttributes.destination;
+        TravelPlan.call.push(this);
+    }
+     
+}
